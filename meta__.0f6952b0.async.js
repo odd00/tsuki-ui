e59250391d7c24da469ffad0d3fef2f94374455a
odd00 <odd00@users.noreply.github.com>
2026-02-25 12:38:08 +0000

    deploy: c7df0d73734b888c5bf0c669a553f512d3bd0c3c

---
+++ meta__.0f6952b0.async.js
@@ -0,0 +1,356 @@
+"use strict";(self.webpackChunktsuki_ui_docs=self.webpackChunktsuki_ui_docs||[]).push([[390],{58836:function(a,e,n){n.r(e),n.d(e,{demos:function(){return o}});var t=n(44194),o={"alert-demo-basic":{component:t.memo(t.lazy(function(){return n.e(819).then(n.bind(n,54435))})),asset:{type:"BLOCK",id:"alert-demo-basic",refAtomIds:["alert"],dependencies:{"index.tsx":{type:"FILE",value:n(37815).Z},react:{type:"NPM",value:"18.3.1"},"../style.ts":{type:"FILE",value:n(71640).Z},"...tsx":{type:"FILE",value:n(56987).Z},"./index.less":{type:"FILE",value:n(21948).Z},"prop-types":{type:"NPM",value:"15.8.1"},clsx:{type:"NPM",value:"2.1.1"}},entry:"index.tsx"},context:void 0,renderOpts:void 0}}},81446:function(a,e,n){n.r(e),n.d(e,{demos:function(){return o}});var t=n(44194),o={"button-demo-basic":{component:t.memo(t.lazy(function(){return n.e(219).then(n.bind(n,15976))})),asset:{type:"BLOCK",id:"button-demo-basic",refAtomIds:["button"],dependencies:{"index.tsx":{type:"FILE",value:n(16351).Z},react:{type:"NPM",value:"18.3.1"},"../style.ts":{type:"FILE",value:n(62303).Z},"...tsx":{type:"FILE",value:n(72270).Z},"./index.less":{type:"FILE",value:n(47475).Z},clsx:{type:"NPM",value:"2.1.1"}},entry:"index.tsx"},context:void 0,renderOpts:void 0}}},22158:function(a,e,n){n.r(e),n.d(e,{demos:function(){return o}});var t=n(44194),o={"icon-demo-basic":{component:t.memo(t.lazy(function(){return Promise.all([n.e(369),n.e(618)]).then(n.bind(n,98069))})),asset:{type:"BLOCK",id:"icon-demo-basic",refAtomIds:["icon"],dependencies:{"index.tsx":{type:"FILE",value:n(89190).Z},react:{type:"NPM",value:"18.3.1"},"@fortawesome/free-solid-svg-icons":{type:"NPM",value:"7.2.0"},"../style.ts":{type:"FILE",value:n(73942).Z},"...tsx":{type:"FILE",value:n(24973).Z},"./index.less":{type:"FILE",value:n(85934).Z},clsx:{type:"NPM",value:"2.1.1"},"@fortawesome/react-fontawesome":{type:"NPM",value:"3.2.0"}},entry:"index.tsx"},context:void 0,renderOpts:void 0}}},89326:function(a,e,n){n.r(e),n.d(e,{texts:function(){return t}});const t=[{value:"\u8B66\u544A\u63D0\u793A\uFF0C\u5C55\u73B0\u9700\u8981\u5173\u6CE8\u7684\u4FE1\u606F\u3002",paraId:0,tocIndex:0},{value:"\u5C5E\u6027",paraId:1,tocIndex:3},{value:"\u8BF4\u660E",paraId:1,tocIndex:3},{value:"\u7C7B\u578B",paraId:1,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:3},{value:"kind",paraId:1,tocIndex:3},{value:"\u8B66\u544A\u7C7B\u578B",paraId:1,tocIndex:3},{value:"'info'/'positive'/'negative'/'warning'\u975E\u5FC5\u586B",paraId:1,tocIndex:3},{value:"'info'",paraId:1,tocIndex:3}]},40918:function(a,e,n){n.r(e),n.d(e,{texts:function(){return t}});const t=[{value:"\u6309\u94AE\u7528\u4E8E\u5F00\u59CB\u4E00\u4E2A\u5373\u65F6\u64CD\u4F5C\u3002",paraId:0,tocIndex:0},{value:"\u6807\u8BB0\u4E86\u4E00\u4E2A\uFF08\u6216\u5C01\u88C5\u4E00\u7EC4\uFF09\u64CD\u4F5C\u547D\u4EE4\uFF0C\u54CD\u5E94\u7528\u6237\u70B9\u51FB\u884C\u4E3A\uFF0C\u89E6\u53D1\u76F8\u5E94\u7684\u4E1A\u52A1\u903B\u8F91\u3002",paraId:1,tocIndex:1},{value:"\u5C5E\u6027",paraId:2,tocIndex:4},{value:"\u8BF4\u660E",paraId:2,tocIndex:4},{value:"\u7C7B\u578B",paraId:2,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:4},{value:"btnType",paraId:2,tocIndex:4},{value:"\u8BBE\u7F6E\u6309\u94AE\u7C7B\u578B",paraId:2,tocIndex:4},{value:"primary",paraId:2,tocIndex:4},{value:" | ",paraId:2,tocIndex:4},{value:"default",paraId:2,tocIndex:4},{value:" | ",paraId:2,tocIndex:4},{value:"danger",paraId:2,tocIndex:4},{value:" | ",paraId:2,tocIndex:4},{value:"link",paraId:2,tocIndex:4},{value:"default",paraId:2,tocIndex:4},{value:"size",paraId:2,tocIndex:4},{value:"\u8BBE\u7F6E\u6309\u94AE\u5927\u5C0F",paraId:2,tocIndex:4},{value:"lg",paraId:2,tocIndex:4},{value:" | ",paraId:2,tocIndex:4},{value:"sm",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"disabled",paraId:2,tocIndex:4},{value:"\u6309\u94AE\u5931\u6548\u72B6\u6001",paraId:2,tocIndex:4},{value:"boolean",paraId:2,tocIndex:4},{value:"false",paraId:2,tocIndex:4},{value:"href",paraId:2,tocIndex:4},{value:"\u70B9\u51FB\u8DF3\u8F6C\u7684\u5730\u5740\uFF0C\u6307\u5B9A\u6B64\u5C5E\u6027 button \u7684\u884C\u4E3A\u548C a \u94FE\u63A5\u4E00\u81F4",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"className",paraId:2,tocIndex:4},{value:"\u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"children",paraId:2,tocIndex:4},{value:"\u6309\u94AE\u5185\u5BB9",paraId:2,tocIndex:4},{value:"ReactNode",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4}]},2104:function(a,e,n){n.r(e),n.d(e,{texts:function(){return t}});const t=[{value:"\u57FA\u4E8E react-fontawesome \u5C01\u88C5\u7684\u56FE\u6807\u7EC4\u4EF6\uFF0C\u63D0\u4F9B\u8BED\u4E49\u5316\u7684\u4E3B\u9898\u8272\u548C\u7EDF\u4E00\u7684\u4F7F\u7528\u65B9\u5F0F\u3002",paraId:0,tocIndex:0},{value:"\u9700\u8981\u4F7F\u7528\u56FE\u6807\u8868\u8FBE\u542B\u4E49\u6216\u8F85\u52A9\u64CD\u4F5C\u65F6\u3002",paraId:1,tocIndex:1},{value:"Icon \u7EC4\u4EF6\u7EE7\u627F\u4E86 ",paraId:2,tocIndex:4},{value:"FontAwesomeIconProps",paraId:2,tocIndex:4},{value:" \u7684\u6240\u6709\u5C5E\u6027\uFF08\u5982 ",paraId:2,tocIndex:4},{value:"icon",paraId:2,tocIndex:4},{value:"\u3001",paraId:2,tocIndex:4},{value:"size",paraId:2,tocIndex:4},{value:"\u3001",paraId:2,tocIndex:4},{value:"spin",paraId:2,tocIndex:4},{value:"\u3001",paraId:2,tocIndex:4},{value:"pulse",paraId:2,tocIndex:4},{value:"\u3001",paraId:2,tocIndex:4},{value:"className",paraId:2,tocIndex:4},{value:" \u7B49\uFF09\uFF0C\u4EE5\u4E0B\u4E3A\u6269\u5C55\u5C5E\u6027\uFF1A",paraId:2,tocIndex:4},{value:"\u5C5E\u6027",paraId:3,tocIndex:4},{value:"\u8BF4\u660E",paraId:3,tocIndex:4},{value:"\u7C7B\u578B",paraId:3,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:4},{value:"theme",paraId:3,tocIndex:4},{value:"\u4E3B\u9898\u8272",paraId:3,tocIndex:4},{value:"ThemeType",paraId:3,tocIndex:4},{value:" \u679A\u4E3E\uFF1A",paraId:3,tocIndex:4},{value:"Primary",paraId:3,tocIndex:4},{value:" | ",paraId:3,tocIndex:4},{value:"Success",paraId:3,tocIndex:4},{value:" | ",paraId:3,tocIndex:4},{value:"Warning",paraId:3,tocIndex:4},{value:" | ",paraId:3,tocIndex:4},{value:"Danger",paraId:3,tocIndex:4},{value:" | ",paraId:3,tocIndex:4},{value:"Info",paraId:3,tocIndex:4},{value:"-",paraId:3,tocIndex:4},{value:"\u5C5E\u6027",paraId:4,tocIndex:5},{value:"\u8BF4\u660E",paraId:4,tocIndex:5},{value:"\u7C7B\u578B",paraId:4,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:4,tocIndex:5},{value:"icon",paraId:4,tocIndex:5},{value:"\u56FE\u6807\u7C7B\u578B\uFF08FontAwesome \u56FE\u6807\u5BF9\u8C61\uFF09",paraId:4,tocIndex:5},{value:"IconProp",paraId:4,tocIndex:5},{value:"-\uFF08\u5FC5\u586B\uFF09",paraId:4,tocIndex:5},{value:"size",paraId:4,tocIndex:5},{value:"\u56FE\u6807\u5927\u5C0F",paraId:4,tocIndex:5},{value:"'xs'",paraId:4,tocIndex:5},{value:" | ",paraId:4,tocIndex:5},{value:"'sm'",paraId:4,tocIndex:5},{value:" | ",paraId:4,tocIndex:5},{value:"'lg'",paraId:4,tocIndex:5},{value:" | ",paraId:4,tocIndex:5},{value:"'2x'",paraId:4,tocIndex:5},{value:" | ",paraId:4,tocIndex:5},{value:"'3x'",paraId:4,tocIndex:5},{value:" | ...",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"spin",paraId:4,tocIndex:5},{value:"\u662F\u5426\u65CB\u8F6C",paraId:4,tocIndex:5},{value:"boolean",paraId:4,tocIndex:5},{value:"false",paraId:4,tocIndex:5},{value:"pulse",paraId:4,tocIndex:5},{value:"\u662F\u5426\u8109\u51B2\u65CB\u8F6C",paraId:4,tocIndex:5},{value:"boolean",paraId:4,tocIndex:5},{value:"false",paraId:4,tocIndex:5},{value:"className",paraId:4,tocIndex:5},{value:"\u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:4,tocIndex:5},{value:"string",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"\u672C\u7EC4\u4EF6\u4F7F\u7528 ",paraId:5,tocIndex:6},{value:"@fortawesome/free-solid-svg-icons",paraId:5,tocIndex:6},{value:"\uFF0C\u53EF\u7528\u56FE\u6807\u8BF7\u53C2\u8003 ",paraId:5,tocIndex:6},{value:"Font Awesome \u56FE\u6807\u5E93",paraId:5,tocIndex:6},{value:"\u3002",paraId:5,tocIndex:6},{value:"\u4F7F\u7528\u65F6\u6309\u9700\u5F15\u5165\uFF1A",paraId:6,tocIndex:6},{value:`import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { Icon } from 'tsuki-ui';
+
+<Icon icon={faCoffee} theme="primary" />;
+`,paraId:7,tocIndex:6}]},37815:function(a,e){e.Z=`import React from 'react';
+import Alert from '../';
+import '../style';
+
+export default () => <Alert kind="warning">\u8FD9\u662F\u4E00\u6761\u8B66\u544A\u63D0\u793A</Alert>;
+`},56987:function(a,e){e.Z=`import React from 'react';
+import t from 'prop-types';
+import clsx from 'clsx';
+
+export interface AlertProps {
+  /**
+   * @description       Alert \u7684\u7C7B\u578B
+   * @default           'info'
+   */
+  kind?: 'info' | 'positive' | 'negative' | 'warning';
+  children?: React.ReactNode;
+  className?: string; // Add className prop
+  style?: React.CSSProperties; // Add style prop
+}
+
+const prefixCls = 'tsuki-alert';
+
+const Alert: React.FC<AlertProps> = ({ children, kind = 'info', className, ...rest }) => (
+  <div className={clsx(prefixCls, \`\${prefixCls}-\${kind}\`, className)} {...rest}>
+    {children}
+  </div>
+);
+
+Alert.propTypes = {
+  kind: t.oneOf(['info', 'positive', 'negative', 'warning']),
+};
+
+export default Alert;
+`},21948:function(a,e){e.Z=`@import '../../style/variables.less';
+@import '../../style/mixins.less';
+
+@alert-prefix-cls: ~'@{tsuki-prefix}-alert';
+
+.@{alert-prefix-cls} {
+  .reset-component();
+  padding: 20px;
+  background: white;
+  border-radius: @border-radius-base;
+  color: @white;
+
+  &-info {
+    background-color: @info-color;
+  }
+  &-positive {
+    background-color: @success-color;
+  }
+  &-negative {
+    background-color: @error-color;
+  }
+  &-warning {
+    background-color: @warning-color;
+  }
+}
+`},71640:function(a,e){e.Z=`import './index.less';
+`},16351:function(a,e){e.Z=`import React from 'react';
+import Button, { ButtonType, ButtonSize } from '../';
+import '../style';
+
+export default () => (
+  <>
+    <div>
+      <h3>Button Type</h3>
+      <Button btnType={ButtonType.Default}>Default</Button>
+      <Button btnType={ButtonType.Primary}>Primary</Button>
+      <Button btnType={ButtonType.Danger}>Danger</Button>
+      <Button btnType={ButtonType.Link} href="https://google.com">
+        Link
+      </Button>
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <h3>Button Size</h3>
+      <Button size={ButtonSize.Large}>Large</Button>
+      <Button>Default</Button>
+      <Button size={ButtonSize.Small}>Small</Button>
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <h3>Disabled Button</h3>
+      <Button disabled>Disabled Default</Button>
+      <Button btnType={ButtonType.Primary} disabled>
+        Disabled Primary
+      </Button>
+      <Button btnType={ButtonType.Link} href="https://google.com" disabled>
+        Disabled Link
+      </Button>
+    </div>
+  </>
+);
+`},72270:function(a,e){e.Z=`import React, { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react';
+import clsx from 'clsx';
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm',
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link',
+}
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+
+const prefixCls = 'tsuki-btn';
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    className,
+    disabled = false,
+    size,
+    btnType = ButtonType.Default,
+    children,
+    href,
+    ...restProps
+  } = props;
+  const classes = clsx(prefixCls, className, {
+    [\`\${prefixCls}-\${size}\`]: size,
+    [\`\${prefixCls}-\${btnType}\`]: btnType,
+    disabled: btnType === ButtonType.Link && disabled,
+  });
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a className={classes} href={href} {...restProps}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classes} disabled={disabled} {...restProps}>
+        {children}
+      </button>
+    );
+  }
+};
+
+export default Button;
+`},47475:function(a,e){e.Z=`@import '../../style/variables.less';
+@import '../../style/mixins.less';
+
+@btn-prefix-cls: ~'@{tsuki-prefix}-btn';
+
+.@{btn-prefix-cls} {
+  position: relative;
+  display: inline-block;
+  font-weight: 400;
+  white-space: nowrap;
+  text-align: center;
+  background-image: none;
+  border: 1px solid transparent;
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.015);
+  cursor: pointer;
+  .transition();
+  user-select: none;
+  touch-action: manipulation;
+  .btn-size(32px, 4px 15px, @font-size-base);
+  color: rgba(0, 0, 0, 0.85); // Default color
+  background-color: @white; // Default background
+  border-color: #d9d9d9; // Default border
+
+  &:focus,
+  &:hover {
+    color: @primary-color;
+    border-color: @primary-color;
+    background-color: @white;
+  }
+
+  &:active {
+    color: darken(@primary-color, 10%);
+    border-color: darken(@primary-color, 10%);
+    background: @white;
+  }
+
+  &[disabled] {
+    .btn-disabled();
+  }
+
+  &-primary {
+    .btn-solid-variant(@primary-color);
+  }
+
+  &-danger {
+    .btn-solid-variant(@error-color);
+  }
+
+  &-link {
+    color: @primary-color;
+    background-color: transparent;
+    border-color: transparent;
+    box-shadow: none;
+
+    &:hover,
+    &:focus {
+      color: lighten(@primary-color, 10%);
+      border-color: transparent;
+      background: transparent;
+    }
+
+    &:active {
+      color: darken(@primary-color, 10%);
+      border-color: transparent;
+      background: transparent;
+    }
+
+    &.disabled,
+    &[disabled] {
+      color: rgba(0, 0, 0, 0.25);
+      background-color: transparent;
+      border-color: transparent;
+    }
+  }
+
+  &-lg {
+    .btn-size(40px, 6.4px 15px, 16px);
+  }
+
+  &-sm {
+    .btn-size(24px, 0 7px, 14px);
+  }
+}
+`},62303:function(a,e){e.Z=`import './index.less';
+`},89190:function(a,e){e.Z=`import React from 'react';
+import Icon, { ThemeType } from '../';
+import '../style';
+import {
+  faCoffee,
+  faCheck,
+  faSpinner,
+  faTimes,
+  faExclamationTriangle,
+  faInfoCircle,
+  faArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
+
+export default () => (
+  <>
+    <div>
+      <h3>\u57FA\u7840\u56FE\u6807</h3>
+      <Icon icon={faCoffee} style={{ marginRight: 12 }} />
+      <Icon icon={faCheck} style={{ marginRight: 12 }} />
+      <Icon icon={faTimes} style={{ marginRight: 12 }} />
+      <Icon icon={faExclamationTriangle} style={{ marginRight: 12 }} />
+      <Icon icon={faInfoCircle} style={{ marginRight: 12 }} />
+      <Icon icon={faArrowRight} />
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <h3>\u4E3B\u9898\u8272</h3>
+      <Icon icon={faCheck} theme={ThemeType.Primary} style={{ marginRight: 12 }} />
+      <Icon icon={faCheck} theme={ThemeType.Success} style={{ marginRight: 12 }} />
+      <Icon icon={faExclamationTriangle} theme={ThemeType.Warning} style={{ marginRight: 12 }} />
+      <Icon icon={faTimes} theme={ThemeType.Danger} style={{ marginRight: 12 }} />
+      <Icon icon={faInfoCircle} theme={ThemeType.Info} />
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <h3>\u56FE\u6807\u5927\u5C0F</h3>
+      <Icon icon={faCoffee} size="xs" style={{ marginRight: 12 }} />
+      <Icon icon={faCoffee} size="sm" style={{ marginRight: 12 }} />
+      <Icon icon={faCoffee} size="lg" style={{ marginRight: 12 }} />
+      <Icon icon={faCoffee} size="2x" style={{ marginRight: 12 }} />
+      <Icon icon={faCoffee} size="3x" />
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <h3>\u52A8\u753B\u6548\u679C</h3>
+      <Icon icon={faSpinner} spin size="2x" style={{ marginRight: 12 }} />
+      <Icon icon={faSpinner} pulse size="2x" />
+    </div>
+  </>
+);
+`},24973:function(a,e){e.Z=`import React from 'react';
+import clsx from 'clsx';
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
+
+export enum ThemeType {
+  Primary = 'primary',
+  Success = 'success',
+  Warning = 'warning',
+  Danger = 'danger',
+  Info = 'info',
+}
+
+export interface IconProps extends FontAwesomeIconProps {
+  /** \u56FE\u6807\u4E3B\u9898\u8272 */
+  theme?: ThemeType;
+  /** \u81EA\u5B9A\u4E49\u7C7B\u540D */
+  className?: string;
+}
+
+const prefixCls = 'tsuki-icon';
+
+const Icon: React.FC<IconProps> = (props) => {
+  const { className, theme, ...restProps } = props;
+  const classes = clsx(prefixCls, className, {
+    [\`\${prefixCls}-\${theme}\`]: theme,
+  });
+
+  return <FontAwesomeIcon className={classes} {...restProps} />;
+};
+
+export default Icon;
+`},85934:function(a,e){e.Z=`@import '../../style/variables.less';
+@import '../../style/mixins.less';
+
+@icon-prefix-cls: ~'@{tsuki-prefix}-icon';
+
+.@{icon-prefix-cls} {
+  .transition(color, 0.2s);
+
+  // \u4E3B\u9898\u8272
+  &-primary {
+    color: @primary-color;
+  }
+
+  &-success {
+    color: @success-color;
+  }
+
+  &-warning {
+    color: @warning-color;
+  }
+
+  &-danger {
+    color: @error-color;
+  }
+
+  &-info {
+    color: @info-color;
+  }
+}
+`},73942:function(a,e){e.Z=`import './index.less';
+`}}]);
